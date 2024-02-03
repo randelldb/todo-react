5cfcaefe97a2fbb8a283f1eecc84fc39a5a48308
randellbakker <randell@live.nl>
2024-02-03 22:25:16 +0100

    progress

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+function Header(){
+    return (
+        <h1 className={'mb-24'}>React To-Do</h1>
+    )
+}
+
+export default Header;
---
+++ src/components/inputTask/InputTask.tsx
@@ -0,0 +1,11 @@
+function InputTask(){
+    return (
+        <form action="">
+            <p><label htmlFor="task">Task</label></p>
+            <p><input type="text" name="task" id={"task"}/></p>
+            <p><button type={"submit"}>Create</button></p>
+        </form>
+    )
+}
+
+export default InputTask
