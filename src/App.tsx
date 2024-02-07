5aa7bc6d4415c7b6877d79706512a5666c367a02
randellbakker <randell@live.nl>
2024-02-07 23:26:28 +0100

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import Header from "./components/header/Header.tsx";
-import InputTask from "./components/inputTask/InputTask.tsx";
 import TaskList from "./components/taskList/TaskList.tsx";
+
 function App() {
   return (
     <div className='container'>
         <Header />
-        <InputTask />
         <TaskList />
     </div>
   )
--- src/components/taskList/TaskList.tsx
+++ src/components/taskList/TaskList.tsx
@@ -1,24 +1,85 @@
-function Task(){
+import {useEffect, useState} from "react";
+import InputTask from "../inputTask/InputTask.tsx";
+
+
+const TaskList = () => {
+
+    const [tasks, setTasks] = useState([]);
+    const [editing, setEditing] = useState({});
+
+    useEffect(() => {
+        getTasks();
+    }, []);
+
+    const getTasks = async () => {
+        const response = await fetch('http://localhost:8080/tasks');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(data)
+        setTasks(data);
+
+    };
+
+    const deleteTask = async (taskId: string) => {
+        const response = await fetch(`http://localhost:8080/tasks/${taskId}`, {
+            method: 'DELETE', // Specify the method to use
+        });
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        setTasks(currentTasks => currentTasks.filter(task => {
+            return task._id !== taskId;
+        }));
+    }
+
+    const toggleTask = async (taskId: string, state:boolean) => {
+        const response = await fetch(`http://localhost:8080/tasks/${taskId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({state: !state})
+        });
+
+        if (!response.ok){
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        await getTasks();
+        console.log('State updated successfully:', data);
+    }
+
+    const toggleEditing = (id) => {
+        setEditing((prev) => {
+            return ({...prev, [id]: !prev[id]});
+        });
+    };
+
+
+
     return (
-        <div className={"task"}>
-            <div className={"task-desc"}>Boodschappendoen</div>
-            <div className={"task-actions"}>
-                <span>Delete</span>
-                <span>Edit</span>
-                <span>Done</span>
+        <div>
+            <InputTask onTaskCreated={getTasks}/>
+            <div className={"task-list"}>
+                {tasks.map((task: any, index) => (
+                    <div key={index} className={`task ${task.state ? 'done' : ''}`}>
+                        {editing[task._id] ? (<div><input type={"text"}/></div>) : (
+                            <div className={"task-desc"}>
+                                {task.task}
+                            </div>
+                        )}
+                        <div className={"task-actions"}>
+                            <span onClick={() => toggleTask(task._id, task.state)}>{task.state ? 'Uncheck' : 'Check'}</span>
+                            <span onClick={() => toggleEditing(task._id)}>Edit</span>
+                            <span onClick={() => deleteTask(task._id)}>X</span>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
-    );
-}
-
-function TaskList(){
-    return (
-        <div className={"task-list"}>
-            <Task />
-            <Task />
-            <Task />
-        </div>
-    )
+)
 }
 
 export default TaskList;
